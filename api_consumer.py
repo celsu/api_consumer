026471e627566a5d92402d18f2b18dd6fedf6172
Celso Machado <celsobarrosmachado@gmail.com>
2024-04-18 22:09:11 +0100

    first release python

---
+++ api_consumer.py
@@ -0,0 +1,19 @@
+import requests
+
+# Definindo a URL da API
+url = 'https://jsonplaceholder.typicode.com/posts'
+
+# Fazendo a requisição GET à API
+response = requests.get(url)
+
+# Verificando se a requisição foi besm-sucedida (código 200)
+if response.status_code == 200:
+    # Convertendo os dados da resposta para formato JSON
+    data = response.json()
+    
+    # Iterando sobre os posts e imprimindo seus títulos
+    for post in data:
+        print("Titulo: "+post['title'])
+else:
+    # Se a requisição não foi bem-sucedida, imprimir o código de status
+    print(f"Erro ao acessar a API. Código de status: {response.status_code}")
